494f3e71e9d2740c758e269329d5e179003364b7
LevaniUgulava <133241911+LevaniUgulava@users.noreply.github.com>
2026-04-11 01:15:09 +0400

    feat: add default layout and modal state control

---
+++ app/layouts/default.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="app-wrapper">
+        <header-component @open-auth-modal="toggleModal('auth', true)"
+            @open-profile-modal="toggleModal('profile', true)" @open-enrolled-modal="toggleModal('enrollment', true)" />
+        <main class="content-canvas">
+            <slot />
+        </main>
+        <footer-component />
+    </div>
+</template>
+<script setup lang="ts">
+
+
+
+
+const modalState = reactive({
+    auth: { isOpen: false, type: null as 'login' | 'register' | null },
+    profile: false,
+    enrollment: false,
+});
+
+const toggleModal = (modalName: keyof typeof modalState, value: boolean, type?: 'login' | 'register' | null) => {
+    if (modalName === 'auth') {
+        modalState.auth.isOpen = value;
+        if (type) modalState.auth.type = type;
+    } else {
+        (modalState[modalName] as boolean) = value;
+    }
+};
+
+</script>
+
+<style>
+html,
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Inter', sans-serif;
+    height: 100%;
+
+}
+
+.app-wrapper {
+    display: flex;
+    /* ჩავრთოთ flexbox */
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+    background-color: #F5F5F5;
+    align-items: flex-start;
+    margin: 0;
+    padding: 0;
+}
+
+.content-canvas {
+    width: 1920px;
+    /* height: 1080px; */
+    flex: 1 0 auto;
+    background-color: #F5F5F5;
+    position: relative;
+    flex-shrink: 0;
+    overflow: hidden;
+    padding-bottom: 100px;
+}
+</style>
